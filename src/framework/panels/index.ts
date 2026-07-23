83e63143d64c52e6b1073d871473bc9adb026c12
Deepak Singh <deepranu01@gmail.com>
2026-07-24 04:37:30 +0530

    R7.5-M2-S2: create panel collection framework

--- src/framework/panels/index.ts
+++ src/framework/panels/index.ts
@@ -1,2 +1,4 @@
 export * from "@/src/framework/panels/InstitutionalPanel";
+export * from "@/src/framework/panels/InstitutionalPanelCollection";
+export * from "@/src/framework/panels/PanelCollectionMetadata";
 export * from "@/src/framework/panels/PanelMetadata";
