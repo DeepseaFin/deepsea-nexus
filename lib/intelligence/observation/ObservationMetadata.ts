20f431fa609da146440e862df0554566e80d8925
Deepak Singh <deepranu01@gmail.com>
2026-07-24 03:37:41 +0530

    R5-M3-S1: create observation domain foundation

---
+++ lib/intelligence/observation/ObservationMetadata.ts
@@ -0,0 +1,6 @@
+export interface ObservationMetadata {
+  readonly sourceSystem?: string;
+  readonly sourceReference?: string;
+  readonly tags?: readonly string[];
+  readonly attributes?: Readonly<Record<string, string | number | boolean>>;
+}
---
+++ lib/intelligence/observation/index.ts
@@ -0,0 +1,7 @@
+export * from "@/lib/intelligence/observation/Observation";
+export * from "@/lib/intelligence/observation/ObservationId";
+export * from "@/lib/intelligence/observation/ObservationMetadata";
+export * from "@/lib/intelligence/observation/ObservationRepository";
+export * from "@/lib/intelligence/observation/ObservationService";
+export * from "@/lib/intelligence/observation/ObservationStatus";
+export * from "@/lib/intelligence/observation/ObservationType";
