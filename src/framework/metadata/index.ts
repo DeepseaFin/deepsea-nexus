d401ab4302143746297ae548e090e579444e958d
Deepak Singh <deepranu01@gmail.com>
2026-07-24 13:57:09 +0530

    R7.5-M5-S2: create metadata provider framework

--- src/framework/metadata/index.ts
+++ src/framework/metadata/index.ts
@@ -1,2 +1,4 @@
 export * from "@/src/framework/metadata/InstitutionalMetadata";
+export * from "@/src/framework/metadata/MetadataProvider";
+export * from "@/src/framework/metadata/MetadataReference";
 export * from "@/src/framework/metadata/InstitutionalVersion";
